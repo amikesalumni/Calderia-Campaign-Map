b1a36cc96ffe2f2928215d05051c26b976a3a71b
amikesalumni <amikes@alumnni.nd.edu>
2023-02-20 22:11:32 -0500

    Session 23 map update + calendar providing campaign timeline

---
+++ calendar.js
@@ -0,0 +1,198 @@
+/**
+ * Calendar block
+ */
+let events = [
+  { type: "Session", month: 3, date: 1 },
+  { type: "Session", month: 3, date: 8 },
+  { type: "Session", month: 3, date: 12 },
+  { type: "Session", month: 3, date: 17 },
+  { type: "Session", month: 3, date: 22 },
+  { type: "Session", month: 3, date: 26 },
+  { type: "development", month: 3, date: 27 },
+  { type: "Session", month: 4, date: 1 },
+  { type: "Session", month: 4, date: 8 },
+  { type: "Session", month: 4, date: 15 },
+  { type: "Session", month: 4, date: 25 },
+  { type: "Session", month: 5, date: 1 },
+  { type: "Session", month: 5, date: 8 },
+  { type: "Session", month: 5, date: 15 },
+  { type: "Session", month: 5, date: 22 },
+  { type: "development", month: 5, date: 25 },
+  { type: "Session", month: 6, date: 1 },
+  { type: "Session", month: 6, date: 8 },
+  { type: "Session", month: 6, date: 12 },
+  { type: "Session", month: 6, date: 15 },
+  { type: "Session", month: 6, date: 22 },
+  { type: "development", month: 6, date: 26 },
+  { type: "Session", month: 7, date: 1 },
+  { type: "development", month: 7, date: 7 },
+  { type: "Session", month: 7, date: 8 },
+  { type: "Session", month: 7, date: 15 },
+  { type: "Session", month: 7, date: 22 },
+  { type: "Session", month: 8, date: 1 },
+];
+
+var year = 4647
+let mo1to4 = ["Zarantyr", "Olarune", "Therendor","Eyre"]
+let mo5to8 = ["Dravago", "Nymm", "Lharvion", "Barrakas"]
+let mo9to12 = ["Rhaan", "Sypheros", "Aryth", "Vult"]
+let days = ["U","M","T","W","R","F","S"]
+
+function generateTableHead(table, data) {
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+  for (let key of data) {
+    let th = document.createElement("th");
+    let text = document.createTextNode(key);
+    th.appendChild(text);
+    th.setAttribute("colspan", 7)
+    row.appendChild(th);
+  }
+}
+
+function generateTable(table, data) {
+  let row = table.insertRow();
+  for (let i = 0; i<4; i++){
+    for (let element of data) {
+      let cell = row.insertCell();
+      let text = document.createTextNode(element);
+      cell.appendChild(text);
+    }
+  }
+  let row2 = table.insertRow();
+  for (let i = 0; i<4; i++){
+    for (let j=1; j<8; j++) {
+      let cell = row2.insertCell();
+      let text = document.createTextNode(j);
+      cell.appendChild(text);
+      cell.id = table.id + 30*i+j
+    }
+  }
+  let row3 = table.insertRow();
+  for (let i = 0; i<4; i++){
+    for (let j=8; j<15; j++) {
+      let cell = row3.insertCell();
+      let text = document.createTextNode(j);
+      cell.appendChild(text);
+      cell.id = table.id + 30*i+j
+    }
+  }
+  let row4 = table.insertRow();
+  for (let i = 0; i<4; i++){
+    for (let j=15; j<22; j++) {
+      let cell = row4.insertCell();
+      let text = document.createTextNode(j);
+      cell.appendChild(text);
+      cell.id = table.id + 30*i+j
+    }
+  }
+  let row5 = table.insertRow();
+  for (let i = 0; i<4; i++){
+    for (let j=22; j<29; j++) {
+      let cell = row5.insertCell();
+      let text = document.createTextNode(j);
+      cell.appendChild(text);
+      cell.id = table.id + 30*i+j
+    }
+  }
+}
+
+function highlightdate(data) {
+  const dates = []
+  for (let element of data){
+    // select the color for the highlight based on the event type
+    if(element['type'] == "development"){
+      var color = 'gold'
+    } else { var color = 'lightgreen' }
+    // select which table will be edited in the highlight based on the month
+    var m = element['month']
+    if(m==3 || m==4){
+      tableselect = "mo1-4"
+    } else if(m==5 || m==6 || m==7 || m==8){
+      tableselect = "mo5-8"
+    } else {tableselect = "mo9-12"}
+    //console.log(element['type'], element['month'], element['date'])
+    var targetdate = document.getElementById(tableselect + 30*((element['month']-1)%4) + element['date'])
+    targetdate.className += color
+    //targetdate.style.backgroundColor = color // permanently sets a highlight, overriding style sheet
+    targetdate.style.fontWeight = 'bold'
+    dates.push(targetdate)
+  }
+  return dates
+}
+
+let table = document.getElementById("mo1-4");
+generateTable(table, days);
+generateTableHead(table, mo1to4);
+let table2 = document.getElementById("mo5-8");
+generateTable(table2, days);
+generateTableHead(table2, mo5to8);
+let table3 = document.getElementById("mo9-12");
+generateTable(table3, days);
+generateTableHead(table3, mo9to12);
+var x = highlightdate(events)
+
+// bind summaries and names to highlighted dates for click events
+var gold_desc = [
+  'Official communication from Fausta',
+  'Reinforcements Arrive',
+  'Amlid arrives on the island',
+  'Sariel Joins the party'
+]
+
+const names = []
+const summaries = []
+
+console.log(parseFloat(region_pillar.features[15].properties.markernames[0].substring(8,10)))
+for (let i = 0; i<region_pillar.features.length; i++){
+  for (let j=0; j<region_pillar.features[i].properties.markernames.length;j++){
+    names.push(region_pillar.features[i].properties.markernames[j])
+    summaries.push(region_pillar.features[i].properties.markersummary[j])
+  }
+}
+
+function swapElements(array,i1,i2){
+  [array[i1],array[i2]] = [array[i2],array[i1]]
+}
+//sorting names and summaries so they're in the correct order
+var order = 1
+while(order == 1){
+  order = 0
+  for (let k=0; k<names.length; k++){
+    if(k>0 && parseFloat(names[k].substring(8,10)) < parseFloat(names[k-1].substring(8,10))){
+      swapElements(names,k,(k-1))
+      swapElements(summaries,k,(k-1))
+      order = 1
+    }
+  }
+}
+
+function ondateclick(y,z,a) {
+  var count = -1
+  var doubt = -1
+  for (let i = 0; i<x.length; i++){
+    if(x[i].className == 'gold'){
+      count++
+      for (let j = 0; j<y.length; j++){
+        if(j==count){
+          x[i].addEventListener('click',(event) =>{
+            document.getElementById("marker_name").innerHTML = '';
+            document.getElementById("summary").innerHTML = '<p>' + y[j] + '</p>';
+          })
+        }
+      }
+    }
+    else if(x[i].className == 'lightgreen'){
+      doubt++
+      for(let k = 0; k<z.length ; k++){ 
+        if(k==doubt){
+          x[i].addEventListener('click',(event) =>{
+            document.getElementById("marker_name").innerHTML = '<h6>' + z[k] + '</h6>';
+            document.getElementById("summary").innerHTML = '<p>' + a[k] + '</p>';
+          })
+        }
+      }
+    }
+  };
+}
+ondateclick(gold_desc,names,summaries)
--- main.js
+++ main.js
@@ -1,3 +1,44 @@
+// button functions
+document.getElementById("Map_button").onclick = function() {mapfunction()};
+function mapfunction() {
+  var x = document.getElementById("map");
+  var y = document.getElementById("calendar");
+  var z = document.getElementById("shop");
+  x.style.display = "block";
+  y.style.display = "none"
+  z.style.display = "none"
+  document.getElementById('explore').innerHTML = 'Click on a Region or Marker'
+  document.getElementById("marker_name").innerHTML = '';
+  document.getElementById("summary").innerHTML = '';
+}
+
+document.getElementById("Calendar_button").onclick = function() {calendarFunction()};
+function calendarFunction() {
+  var x = document.getElementById("map");
+  var y = document.getElementById("calendar");
+  var z = document.getElementById("shop");
+  x.style.display = "none";
+  y.style.display = "block"
+  z.style.display = "none"
+  document.getElementById('explore').innerHTML = 'Click on a Date'
+  document.getElementById("region_name").innerHTML = '';
+  document.getElementById("marker_name").innerHTML = '';
+  document.getElementById("summary").innerHTML = '';
+}
+/*
+document.getElementById("Shop_button").onclick = function() {shopFunction()};
+function shopFunction() {
+  var x = document.getElementById("map");
+  var y = document.getElementById("calendar");
+  var z = document.getElementById("shop");
+  x.style.display = "none";
+  y.style.display = "none"
+  z.style.display = "block"
+}
+*/
+/**
+ * MAPPING INFORMATION
+ */
 // initialize the map
 var bounds = [[0,0], [8192,8192]];
 var map = L.map('map', {
@@ -5,10 +46,11 @@ var map = L.map('map', {
   zoomSnap: 0.25,
   minZoom: -3.5,
   maxBounds: bounds, // might have to set larger bounds than actually present for map if I want to prevent scrolling off
-  maxBoundsViscosity: 1.0 // prevent use from scrolling off map, tentatively less useful without map margins
+  maxBoundsViscosity: 1.0, // prevent use from scrolling off map, tentatively less useful without map margins
+  preferCanvas: true // for some reason this prevents the polygons from lagging when you zoom in on the map
 });
 
-var image = L.imageOverlay('./Player Release 11.jpg', bounds).addTo(map);
+var image = L.imageOverlay('./Player Release 12.jpg', bounds).addTo(map);
 map.fitBounds(bounds);
 
 // adds gimp created path to the map
@@ -64,13 +106,7 @@ function onEachFeature(feature, layer) {
       mouseout: resetHighlight,
       click: zoomToFeature,
   });}
-//custom markers
-/*
-var sessionmarker = L.AwesomeMarkers.icon({
-  icon: 'home',
-  color: 'red'
-})
-*/
+
 // add all the markers to their own layer group and put them on the map
 var mlayer = new L.LayerGroup()
 var props = region_pillar.features;
@@ -86,14 +122,16 @@ for(i=0; i<props.length; i++) {
     marker.coord = coord
     marker.addTo(mlayer)
   }};
-
 map.addLayer(mlayer);
+
+// control for toggling regions and markers
 var markers ={
   "Sessions": mlayer,
   "Regions" : geojson
 };
-
 var layerControl = L.control.layers(null,markers).addTo(map);
+
+// populates detail on the left with information for the corresponding marker when clicked
 function markerclick(e) {
   document.getElementById("region_name").innerHTML = '<h4>'+ this.region + '</h4>';
   document.getElementById("marker_name").innerHTML = '<h6>'+ this.name + '</h6>';
@@ -101,27 +139,7 @@ function markerclick(e) {
   map.setView(this.coord, zoom=0);
 }
 
-// i see, the red triangle appears next to a prior deletion for some reason
-// this provides a useful debugging thing because I can output properties into the legend and see if they change
-// correctly on a click or not
-/*
-var info = L.control();
-info.onAdd = function (map) {
-    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
-    this.update();
-    return this._div;
-};
-
-info.update = function (props) {
-  this._div.innerHTML = '<h4>Region Information </h4>' +  (props ?
-      '<b>' + props.name + '</b><br />': 'click on something');
-};
-
-info.addTo(map);
-
-var scale = L.control.scale({ metric: false});
-scale.addTo(map);
-*/
+//distance measurement tool 
 L.control.measure({
   position: 'topright',
   lineColor: 'white',
@@ -131,93 +149,3 @@ L.control.measure({
   }
 }).addTo(map)
 
-/*
-var plot = {
-    "$schema": "https://vega.github.io/schema/vega/v5.json",
-    "description": "A basic pie chart example.",
-    "width": 200,
-    "height": 200,
-    "autosize": "none",
-  
-    "signals": [
-      {
-        "name": "startAngle", "value": 0,
-        //"bind": {"input": "range", "min": 0, "max": 6.29, "step": 0.01}
-      },
-      {
-        "name": "endAngle", "value": 6.29,
-        //"bind": {"input": "range", "min": 0, "max": 6.29, "step": 0.01}
-      },
-      {
-        "name": "padAngle", "value": 0,
-        //"bind": {"input": "range", "min": 0, "max": 0.1}
-      },
-      {
-        "name": "innerRadius", "value": 0,
-        //"bind": {"input": "range", "min": 0, "max": 90, "step": 1}
-      },
-      {
-        "name": "cornerRadius", "value": 0,
-        //"bind": {"input": "range", "min": 0, "max": 10, "step": 0.5}
-      },
-      {
-        "name": "sort", "value": false,
-        //"bind": {"input": "checkbox"}
-      }
-    ],
-  
-    "data": [
-      {
-        "name": "table",
-        "values": [
-          {"id": 1, "field": 4},
-          {"id": 2, "field": 6},
-          {"id": 3, "field": 10},
-          {"id": 4, "field": 3},
-          {"id": 5, "field": 7},
-          {"id": 6, "field": 8}
-        ],
-        "transform": [
-          {
-            "type": "pie",
-            "field": "field",
-            "startAngle": {"signal": "startAngle"},
-            "endAngle": {"signal": "endAngle"},
-            "sort": {"signal": "sort"}
-          }
-        ]
-      }
-    ],
-  
-    "scales": [
-      {
-        "name": "color",
-        "type": "ordinal",
-        "domain": {"data": "table", "field": "id"},
-        "range": {"scheme": "category20"}
-      }
-    ],
-  
-    "marks": [
-      {
-        "type": "arc",
-        "from": {"data": "table"},
-        "encode": {
-          "enter": {
-            "fill": {"scale": "color", "field": "id"},
-            "x": {"signal": "width / 2"},
-            "y": {"signal": "height / 2"}
-          },
-          "update": {
-            "startAngle": {"field": "startAngle"},
-            "endAngle": {"field": "endAngle"},
-            "padAngle": {"signal": "padAngle"},
-            "innerRadius": {"signal": "innerRadius"},
-            "outerRadius": {"signal": "width / 2"},
-            "cornerRadius": {"signal": "cornerRadius"}
-          }
-        }
-      }
-    ]
-  }
-  */
